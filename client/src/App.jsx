85acfa3ed3f25c06cee4b04a0ce21b020bc86c1c
KODIYARASU01 <kodiyarasu02gmail.com>
2024-04-30 01:44:15 +0530

    DigitalCard 9 client side fully done

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import NewCardDesign9 from './Component/NewCardDesign9';
+
+const App = () => {
+  return (
+    <div>
+<NewCardDesign9/>
+    </div>
+  )
+}
+
+export default App;
